e749220e621d833fd6fea505587404de965e8983
Mohammad Omari <mohammad.omari852@gmail.com>
2025-01-02 05:41:42 +0300

    Create fun_facts.py

---
+++ fun-projects/fun_facts.py
@@ -0,0 +1,11 @@
+import random
+
+facts = [
+    "Raspberry Pi was first released in 2012.",
+    "The first version of Linux was created by Linus Torvalds in 1991.",
+    "A group of penguins on land is called a waddle.",
+    "Your Raspberry Pi can power a small robot!"
+]
+
+print("Did you know?")
+print(random.choice(facts))
